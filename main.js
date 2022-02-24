adae797d266fa53b6d9bffc44945796a1f43eed0
Anaïs Velcker <90798948+AnaisVlr@users.noreply.github.com>
2022-02-24 17:04:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
 const P = new Pokedex.Pokedex()
 
 const app = Vue.createApp({
-    data() {
-        return {
-            id: null,
-            name:'',
-            ability: '',
-        }
-    },
-    methods: {
-    },
-    computed:{
-        
-    }
 })
